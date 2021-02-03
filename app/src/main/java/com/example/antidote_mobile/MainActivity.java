60ebb3cf1944fd68ad5ff355e8d770c94afb2465
fbarry <55502851+fbarry@users.noreply.github.com>
2021-02-03 12:37:40 -0500

    Initial commit

---
+++ app/src/main/java/com/example/antidote_mobile/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.antidote_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
