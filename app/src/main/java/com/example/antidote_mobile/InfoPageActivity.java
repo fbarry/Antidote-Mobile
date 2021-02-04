f11aa80fd01fc1110451f6958ca462bd7fb13164
JakeSteinebronn <jakesteinebronn@gmail.com>
2021-02-03 20:51:11 -0500

    Partially populate info page

--- app/src/main/java/com/example/antidote_mobile/InfoPageActivity.java
+++ app/src/main/java/com/example/antidote_mobile/InfoPageActivity.java
@@ -1,23 +1,41 @@
 package com.example.antidote_mobile;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ScrollView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InfoPageActivity extends AppCompatActivity {
 
+
+    ScrollView scrollView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_page);
+        scrollView = findViewById(R.id.infoScroll);
     }
 
     public void goBack(View v) {
         InfoPageActivity.this.finish();
     }
+
+    public void scrollToSetup(View v){
+        View setupHeader = findViewById(R.id.header_setup);
+        scrollView.smoothScrollTo(0, setupHeader.getTop());
+    }
+
+    public void scrollToObjective(View v){
+        View setupHeader = findViewById(R.id.header_objective);
+        scrollView.smoothScrollTo(0, setupHeader.getTop());
+    }
+
+    public void scrollToGameplay(View v){
+        View setupHeader = findViewById(R.id.header_gameplay);
+        scrollView.smoothScrollTo(0, setupHeader.getTop());
+    }
+
 
 
 }
