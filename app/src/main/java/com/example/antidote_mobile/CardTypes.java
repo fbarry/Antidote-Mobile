f90e274a04c5e1957fd4953a499e397cf7a286c0
JakeSteinebronn <jakesteinebronn@gmail.com>
2021-02-06 11:33:44 -0500

    Add enums for CardTypes and Toxins

---
+++ app/src/main/java/com/example/antidote_mobile/CardTypes.java
@@ -0,0 +1,7 @@
+package com.example.antidote_mobile;
+
+public enum CardTypes {
+    TOXIN,
+    SYRINGE,
+    ANTIDOTE
+}
---
+++ app/src/main/java/com/example/antidote_mobile/Toxins.java
@@ -0,0 +1,12 @@
+package com.example.antidote_mobile;
+
+public enum Toxins {
+    SERUMN,
+    OSLERSOIL,
+    RUBIMAXB,
+    C9TONIC,
+    BOOTHEIDE,
+    W2ROSE,
+    MXVILE,
+    AGENTU
+}
