47b9fa7f3f3819a351da7a504e5e041ac0882213
JakeSteinebronn <jakesteinebronn@gmail.com>
2021-02-06 12:12:38 -0500

    Add some string-enum conversion to Toxin and CardType

--- app/src/main/java/com/example/antidote_mobile/CardType.java
+++ app/src/main/java/com/example/antidote_mobile/CardType.java
@@ -1,8 +1,28 @@
 package com.example.antidote_mobile;
 
 public enum CardType {
-    TOXIN,
-    SYRINGE,
-    ANTIDOTE,
-    NONE
+    TOXIN("TOXIN"),
+    SYRINGE("SYRINGE"),
+    ANTIDOTE("ANTIDOTE"),
+    NONE("NONE");
+
+
+    private String text;
+
+    CardType(String s) {
+        this.text = s;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public static CardType fromString(String text) {
+        for (CardType t : CardType.values()) {
+            if (t.text.equalsIgnoreCase(text)) {
+                return t;
+            }
+        }
+        return NONE;
+    }
 }
--- app/src/main/java/com/example/antidote_mobile/Toxin.java
+++ app/src/main/java/com/example/antidote_mobile/Toxin.java
@@ -2,13 +2,33 @@ package com.example.antidote_mobile;
 
 @SuppressWarnings("unused")
 public enum Toxin {
-    SERUMN,
-    OSLERSOIL,
-    RUBIMAXB,
-    C9TONIC,
-    BOOTHEIDE,
-    W2ROSE,
-    MXVILE,
-    AGENTU,
-    NONE
+    SERUMN("SERUM-N"),
+    OSLERSOIL("OSLER'S OIL"),
+    RUBIMAXB("RUBIMAXB"),
+    C9TONIC("C9-TONIC"),
+    BOOTHEIDE("BOOTHEIDE"),
+    W2ROSE("W²ROSE"),
+    MXVILE("MX-VILE"),
+    AGENTU("AGENT-U"),
+    NONE("NONE");
+
+    private String text;
+
+    Toxin(String s) {
+        this.text = s;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public static Toxin fromString(String text) {
+        for (Toxin t : Toxin.values()) {
+            if (t.text.equalsIgnoreCase(text)) {
+                return t;
+            }
+        }
+        return NONE;
+    }
+
 }
