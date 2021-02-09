ce4aad9960ed161fd812238e7ff820c4ee0ea815
JakeSteinebronn <jakesteinebronn@gmail.com>
2021-02-09 12:18:39 -0500

    move unused code to codesnippets.md and fix some warnings

--- app/src/main/java/com/example/antidote_mobile/MainActivity.java
+++ app/src/main/java/com/example/antidote_mobile/MainActivity.java
@@ -1,28 +1,15 @@
 package com.example.antidote_mobile;
 
 import android.annotation.SuppressLint;
-import android.app.ActionBar;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.parse.FindCallback;
-import com.parse.GetCallback;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,8 +30,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(MainActivity.this, InfoPageActivity.class));
     }
 
+    @SuppressWarnings("unused")
     @SuppressLint("SetTextI18n")
-    public void testytest(View v) throws ParseException {
+    public void testytest(View v) {
         // Put some code here if you want to test something from the home screen.
         TextView textView = findViewById(R.id.textView);
         TextView textView2 = findViewById(R.id.textView2);
@@ -52,122 +40,7 @@ public class MainActivity extends AppCompatActivity {
         if (AntidoteMobile.currentUser != null) {
             textView.setText(AntidoteMobile.currentUser.username + "," + AntidoteMobile.currentUser.email);
 
-//        if (currentUser != null) {
-//            textView.setText(currentUser.getInt("magicnumber") + " is the magic number of " + currentUser.getUsername());
-//        } else {
-//            textView.setText("you silly billy, you're not logged in");
-//            return;
-//
-//        }
-
-//        currentUser.put("magicnumber", (int) (Math.random() * 100));
-//        currentUser.saveInBackground(new SaveCallback() {
-//            @Override
-//            public void done(ParseException e) {
-////                textView2.setText("Update done! ");
-//            }
-//        });
-//        Object thing = currentUser.get("specialArray");
-
-//        if (thing == null) {
-//            textView2.setText("oh noes");
-//        } else {
-//            textView2.setText(thing.toString() + "," + thing.getClass());
-
-//            textView2.append(" "+po.getString("type"));
-//            textView2.append(((ParseObject)thing).get("type").toString());
-//            ArrayList<Integer> trythis = (ArrayList<Integer>) thing;
-//            textView2.setText(textView2.getText()+"/"+trythis.get(0));
         }
-
-//        ParseQuery<ParseUser> query = ParseUser.getQuery();
-//        query.include("theCard");
-//        query.whereEqualTo("username", currentUser.getUsername());
-//        query.findInBackground(new FindCallback<ParseUser>() {
-//            public void done(List<ParseUser> users, ParseException e) {
-//                if (e == null) {
-//                    // The query was successful, returns the users that matches
-//                    // the criterias.
-//                    for(ParseUser user : users) {
-//                        textView2.append(" "+user.getUsername());
-//                        ParseObject card = (ParseObject) user.get("theCard");
-//                        textView2.append(" "+card.getString("type"));
-//
-//                        System.out.println(user.getUsername());
-//
-//                    }
-//                } else {
-//                    // Something went wrong.
-//                }
-//            }
-//        });
-
-
-//
-//        ParseQuery<ParseObject> query = ParseQuery.getQuery("Card");
-//        query.getInBackground("ImFD78s4so", new GetCallback<ParseObject>() {
-//            @Override
-//            public void done(ParseObject object, ParseException e) {
-//                if (e == null) {
-//
-//
-//                    textView2.append(" "+object.toString());
-//
-//                    String display = "SUCC ";
-//                    int cardnum = object.getInt("cardNum");
-//                    String type = object.getString("type");
-//                    display += cardnum + " ";
-//                    display += type;
-////                    textView2.setText(display);
-//                } else {
-//                    textView2.setText(e.getMessage());
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-
-
-//        User newUser = new User("randomUser3", "randomPassword", new SignUpCallback() {
-////            @Override
-////            public void done(ParseException e) {
-////                if (e == null) {
-////                    // Success
-////                    textView.setText("SUCCESS");
-////                } else {
-////                    // Failure
-////                    textView.setText(e.getMessage());
-////                    e.printStackTrace();
-////                }
-////            }
-////        });
-//        ParseQuery<ParseObject> query = ParseQuery.getQuery("Card");
-//        List<ParseObject> thelist = query.find();
-//
-//        StringBuilder disp = new StringBuilder();
-//        for (ParseObject object : thelist) {
-//            disp.append(object.getObjectId()).append(" ");
-//        }
-//        textView2.setText(disp.toString());
-//
-//
-//        //noinspection Convert2Lambda
-//        query.getInBackground("PK7N8AL2H3", new GetCallback<ParseObject>() {
-//            @Override
-//            public void done(ParseObject object, ParseException e) {
-//                if (e == null) {
-//                    String display = "SUCC ";
-//                    int cardnum = object.getInt("cardNum");
-//                    String type = object.getString("type");
-//                    display += cardnum + " ";
-//                    display += type;
-//                    textView.setText(display);
-//                } else {
-//                    textView.setText(e.getMessage());
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-
     }
 
     public void launchLoginWindowDialog(View v) {
@@ -175,10 +48,11 @@ public class MainActivity extends AppCompatActivity {
         myDialog.setContentView(R.layout.login_popup);
         myDialog.setCancelable(true);
         myDialog.setTitle("gaming");
-        Button login = (Button) myDialog.findViewById(R.id.login_loginButton);
+        Button login = myDialog.findViewById(R.id.login_loginButton);
 
         myDialog.show();
 
+        //noinspection Convert2Lambda
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -194,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
                     username.getText().clear();
                     myDialog.dismiss();
                 } else {
-                    message.setText("Login failed.");
+                    message.setText(R.string.login_failed);
                 }
 
             }
--- codesnippets.md
+++ codesnippets.md
@@ -1,1 +1,31 @@
 # Code Snippets
+
+
+## Get a List of all parseObjects with a certain class ID
+ParseQuery<ParseObject> query = ParseQuery.getQuery("Card");
+List<ParseObject> thelist = query.find();
+
+StringBuilder disp = new StringBuilder();
+for (ParseObject object : thelist) {
+    disp.append(object.getObjectId()).append(" ");
+}
+extView2.setText(disp.toString());
+
+## Get a specific row of a database object by ObjectID (In this case, ImFD78s4so of Card)
+ParseQuery<ParseObject> query = ParseQuery.getQuery("Card");
+query.getInBackground("ImFD78s4so", new GetCallback<ParseObject>() {
+    @Override
+    public void done(ParseObject object, ParseException e) {
+        if (e == null) {
+            textView2.append(" "+object.toString());
+
+            int cardnum = object.getInt("cardNum");
+            String type = object.getString("type");
+            display += cardnum + " ";
+            display += type;
+        } else {
+            textView2.setText(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+});
