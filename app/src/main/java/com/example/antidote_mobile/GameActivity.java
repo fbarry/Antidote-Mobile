07b0e0c7fb4f7b97a042acb185100c656c8c0c99
JakeSteinebronn <jakesteinebronn@gmail.com>
2021-02-21 17:09:21 -0500

    Change CardHandler.setCards to take an arraylist of cards and update all the cards

--- app/src/main/java/com/example/antidote_mobile/GameActivity.java
+++ app/src/main/java/com/example/antidote_mobile/GameActivity.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Arrays;
+
 public class GameActivity extends AppCompatActivity {
 
     Game game;
+    CardHandler ch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +17,8 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         game = (Game) getIntent().getSerializableExtra("gameInfo");
-
+        ch = findViewById(R.id.cardHandler);
+        ch.setCards(Arrays.asList("SYRINGE", "TOXIN.SERUM-N", "ANTIDOTE.RUBIMAXB.4", "ANTIDOTE.MX-VILE.2", "SYRINGE"));
 
     }
 
