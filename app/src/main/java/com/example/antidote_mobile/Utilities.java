0e6db6b50f466f444fb5331df597893bce229964
Daniel West <daniel.lomax.west@gmail.com>
2021-02-22 00:15:03 -0500

    Add getRandomInt utility

--- app/src/main/java/com/example/antidote_mobile/Utilities.java
+++ app/src/main/java/com/example/antidote_mobile/Utilities.java
@@ -9,4 +9,10 @@ public class Utilities {
         }
         return sb.toString();
     }
+
+    public static int getRandomInt(int low, int high) {
+        int diff = high - low;
+        int add = (int)(Math.random() * (diff + 1));
+        return low + add;
+    }
 }
