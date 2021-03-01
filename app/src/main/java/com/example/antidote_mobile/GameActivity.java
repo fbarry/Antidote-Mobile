395137b55d95099ab7378bf8df2bf1dfd68d8c74
JakeSteinebronn <jakesteinebronn@gmail.com>
2021-02-28 22:08:15 -0500

    game displays players

--- app/src/main/java/com/example/antidote_mobile/GameActivity.java
+++ app/src/main/java/com/example/antidote_mobile/GameActivity.java
@@ -27,6 +27,41 @@ public class GameActivity extends AppCompatActivity {
         TextView gameCodeTextView = findViewById(R.id.gameCodeTextView);
         gameCodeTextView.append(" " + game.roomCode());
 
+        switch (game.numPlayers()) {
+            case 1:
+            case 2:
+                findViewById(R.id.player3workstation).setVisibility(View.GONE);
+                findViewById(R.id.player3TextView).setVisibility(View.GONE);
+            case 3:
+                findViewById(R.id.player4workstation).setVisibility(View.GONE);
+                findViewById(R.id.player4TextView).setVisibility(View.GONE);
+            case 4:
+                findViewById(R.id.player5workstation).setVisibility(View.GONE);
+                findViewById(R.id.player5TextView).setVisibility(View.GONE);
+            case 5:
+                findViewById(R.id.player6workstation).setVisibility(View.GONE);
+                findViewById(R.id.player6TextView).setVisibility(View.GONE);
+            case 6:
+                findViewById(R.id.player7workstation).setVisibility(View.GONE);
+                findViewById(R.id.player7TextView).setVisibility(View.GONE);
+        }
+        switch (game.numPlayers()) {
+            case 7:
+                ((TextView) findViewById(R.id.player7TextView)).setText(game.players().get(6));
+            case 6:
+                ((TextView) findViewById(R.id.player6TextView)).setText(game.players().get(5));
+            case 5:
+                ((TextView) findViewById(R.id.player5TextView)).setText(game.players().get(4));
+            case 4:
+                ((TextView) findViewById(R.id.player4TextView)).setText(game.players().get(3));
+            case 3:
+                ((TextView) findViewById(R.id.player3TextView)).setText(game.players().get(2));
+            case 2:
+                ((TextView) findViewById(R.id.player2TextView)).setText(game.players().get(1));
+            case 1:
+                ((TextView) findViewById(R.id.player1TextView)).setText(game.players().get(0));
+        }
+
 
     }
 
@@ -43,6 +78,40 @@ public class GameActivity extends AppCompatActivity {
 
     public void openInfoPage(View v) {
         startActivity(new Intent(GameActivity.this, InfoPageActivity.class));
+    }
+
+    public void workstation1(View v) {
+        viewWorkstation(0);
+    }
+
+    public void workstation2(View v) {
+        viewWorkstation(1);
+    }
+
+    public void workstation3(View v) {
+        viewWorkstation(2);
+    }
+
+    public void workstation4(View v) {
+        viewWorkstation(3);
+    }
+
+    public void workstation5(View v) {
+        viewWorkstation(4);
+    }
+
+    public void workstation6(View v) {
+        viewWorkstation(5);
+    }
+
+    public void workstation7(View v) {
+        viewWorkstation(6);
+    }
+
+    // playerNum is zero-indexed
+    @SuppressWarnings("unused")
+    public void viewWorkstation(int playerNum) {
+
     }
 
 }
