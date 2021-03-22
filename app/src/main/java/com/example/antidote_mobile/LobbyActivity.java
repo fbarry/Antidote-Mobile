395f3b9e7c2aaaf7a884eb834a36d48dd93fcdb5
fbarry <55502851+fbarry@users.noreply.github.com>
2021-03-21 23:31:48 -0400

    AI added after refresh

--- app/src/main/java/com/example/antidote_mobile/LobbyActivity.java
+++ app/src/main/java/com/example/antidote_mobile/LobbyActivity.java
@@ -14,8 +14,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.SaveCallback;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Timer;
 
 public class LobbyActivity extends AppCompatActivity implements ChatDialogActivity {
@@ -30,10 +33,14 @@ public class LobbyActivity extends AppCompatActivity implements ChatDialogActivi
     ImageButton chatButton;
     ChatDialog chatDialog;
 
+    ArrayDeque<Player> aiInQueue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+
+        aiInQueue = new ArrayDeque<>();
 
         game = (Game) getIntent().getSerializableExtra("gameInfo");
         currentPlayer = (Player) getIntent().getSerializableExtra("currentPlayer");
@@ -209,12 +216,19 @@ public class LobbyActivity extends AppCompatActivity implements ChatDialogActivi
             for (Player p : list) {
                 if (!copy.contains(p)) adapter.addPlayer(p);
             }
+
+            if (!aiInQueue.isEmpty()) {
+                while (!aiInQueue.isEmpty()) {
+                    game.addPlayer(Objects.requireNonNull(aiInQueue.poll()));
+                }
+
+                game.saveInBackground(e1 -> update());
+            }
         });
     }
 
     public void AIButton(View v) {
-        game.addPlayer(PlayerAI.createPlayerAI());
-        game.saveInBackground(e -> update());
+        aiInQueue.add(PlayerAI.createPlayerAI());
     }
 
     public void leaveGame(View v) {
